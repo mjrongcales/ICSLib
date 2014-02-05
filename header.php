5a9090aa9312eb1106485379752f4f59c3f095b2
mjrongcales <mary.rongcales@gmail.com>
2014-02-05 14:17:34 +0800

    mary: universal header

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.png">
+
+    <title>ICS LIB - CS128</title>
+    
+    <link href="<?=base_url().'css/bootstrap.css'?>" rel="stylesheet" type="text/css"/>
+    <link href="<?=base_url().'css/sample.css'?>" rel="stylesheet" type="text/css"/>
+  </head>
